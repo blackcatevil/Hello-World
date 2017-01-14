8dedd8a596136df96bc144ceca074523d13cdb30
blackcatevil <blackcatevil.cs96@nctu.edu.tw>
2017-01-15 01:59:02 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Howard Chiu
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World and github" << endl;
+	return 0;
+}
